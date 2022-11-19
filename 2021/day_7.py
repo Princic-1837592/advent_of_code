b386e979fdb3ecefe338be334db748f37555b3f6
Princic-1837592 <andreapri.99@gmail.com>
2022-11-19 15:49:44 +0100

    finished 7

--- 2021/day_7.py
+++ 2021/day_7.py
@@ -1,15 +1,29 @@
 def part1(data: str):
-    pass
+    positions = list(map(int, data.split(',')))
+    m, M = min(positions), max(positions)
+    min_sum = M * len(positions)
+    for n in range(m, M + 1):
+        s = sum(map(lambda x: abs(x - n), positions))
+        if s < min_sum:
+            min_sum = s
+    return min_sum
 
 
 def part2(data: str):
-    pass
+    positions = list(map(int, data.split(',')))
+    m, M = min(positions), max(positions)
+    min_sum = M * len(positions) * (M * len(positions) + 1) // 2
+    for n in range(m, M + 1):
+        s = sum(map(lambda x: (abs(x - n) * (abs(x - n) + 1)) // 2, positions))
+        if s < min_sum:
+            min_sum = s
+    return min_sum
 
 
 if __name__ == '__main__':
     test = True
     test = False
-    test_input = ''''''
+    test_input = '''16,1,2,0,4,2,7,1,2,14'''
     if test:
         puzzle_input = test_input
     else:
