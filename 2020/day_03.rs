6442cfcc16c24790f4d563507b238fd3e09a05de
Princic-1837592 <andreapri.99@gmail.com>
2023-01-05 00:34:14 +0100

    day 3

---
+++ 2020/day_03.rs
@@ -0,0 +1,59 @@
+mod part1 {
+    pub(crate) fn solve(input: &str) -> usize {
+        let map: Vec<Vec<_>> = input.lines().map(|l| l.chars().collect()).collect();
+        let mut trees = 0;
+        let mut coord = (0, 0);
+        while coord.0 < map.len() {
+            if map[coord.0][coord.1] == '#' {
+                trees += 1;
+            }
+            coord.0 += 1;
+            coord.1 = (coord.1 + 3) % map[0].len();
+        }
+        trees
+    }
+}
+
+mod part2 {
+    pub(crate) fn solve(input: &str) -> usize {
+        let map: Vec<Vec<_>> = input.lines().map(|l| l.chars().collect()).collect();
+        let mut result = 1;
+        for (r, c) in &[(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)] {
+            let mut trees = 0;
+            let mut coord = (0, 0);
+            while coord.0 < map.len() {
+                if map[coord.0][coord.1] == '#' {
+                    trees += 1;
+                }
+                coord.0 += r;
+                coord.1 = (coord.1 + c) % map[0].len();
+            }
+            result *= trees;
+        }
+        result
+    }
+}
+
+
+fn main() {
+    // let test = true;
+    let test = false;
+    let test_input = "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#".to_owned();
+    let puzzle_input = if test {
+        test_input
+    } else {
+        std::fs::read_to_string("inputs/day_03_input.txt").unwrap()
+    };
+    println!("{}", part1::solve(&puzzle_input));
+    println!("{}", part2::solve(&puzzle_input));
+}
