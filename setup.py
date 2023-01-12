84fbdccbf07dcd864819acb818e6c2b5695dedbd
Princic-1837592 <andreapri.99@gmail.com>
2023-01-12 12:26:13 +0100

    update python template to new format

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     if test:
         puzzle_input = test_input
     else:
-        with open("day_{day:0>2}_input.txt", "r") as input_file:
+        with open("inputs/day_{day:0>2}_input.txt", "r") as input_file:
             puzzle_input = input_file.read().strip()
     print(part1(puzzle_input))
     print(part2(puzzle_input))
