cb71248236f50da37b01668b98fd065bf3438e8d
Princic-1837592 <andreapri.99@gmail.com>
2023-02-18 16:27:04 +0100

    day 8, 9

---
+++ 2017/day_08.rs
@@ -0,0 +1,130 @@
+//! https://adventofcode.com/2017/day/8
+//! https://adventofcode.com/2017/day/8/input
+
+use std::{collections::HashMap, fs::read_to_string, time::Instant};
+
+enum Operator {
+    Eq,
+    Ne,
+    Gt,
+    Get,
+    Lt,
+    Let,
+}
+
+impl From<&str> for Operator {
+    fn from(string: &str) -> Self {
+        match string {
+            "==" => Self::Eq,
+            "!=" => Self::Ne,
+            ">" => Self::Gt,
+            ">=" => Self::Get,
+            "<" => Self::Lt,
+            "<=" => Self::Let,
+            _ => panic!("Invalid operator: {}", string),
+        }
+    }
+}
+
+struct Instruction {
+    dest: String,
+    delta: isize,
+    target: String,
+    operator: Operator,
+    n: isize,
+}
+
+impl From<&str> for Instruction {
+    fn from(string: &str) -> Self {
+        let mut parts = string.split_whitespace();
+        let dest = parts.next().unwrap().to_owned();
+        let delta = if parts.next().unwrap() == "inc" {
+            1
+        } else {
+            -1
+        } * parts.next().unwrap().parse::<isize>().unwrap();
+        let target = parts.nth(1).unwrap().to_owned();
+        let operator = parts.next().unwrap();
+        let n = parts.next().unwrap().parse().unwrap();
+        Instruction {
+            dest,
+            delta,
+            target,
+            operator: Operator::from(operator),
+            n,
+        }
+    }
+}
+
+impl Instruction {
+    fn apply(&self, registers: &HashMap<String, isize>) -> bool {
+        let target = *registers.get(&self.target).unwrap_or(&0);
+        match self.operator {
+            Operator::Eq => target == self.n,
+            Operator::Ne => target != self.n,
+            Operator::Gt => target > self.n,
+            Operator::Get => target >= self.n,
+            Operator::Lt => target < self.n,
+            Operator::Let => target <= self.n,
+        }
+    }
+}
+
+fn parse(input: &str) -> Vec<Instruction> {
+    input.lines().map(Instruction::from).collect()
+}
+
+pub mod part1 {
+    use std::collections::HashMap;
+
+    use crate::day_08::parse;
+
+    pub fn solve(input: &str) -> isize {
+        let instructions = parse(input);
+        let mut registers = HashMap::new();
+        for instruction in instructions {
+            if instruction.apply(&registers) {
+                *registers.entry(instruction.dest).or_insert(0) += instruction.delta;
+            }
+        }
+        *registers.values().max().unwrap()
+    }
+}
+
+pub mod part2 {
+    use std::collections::HashMap;
+
+    use crate::day_08::parse;
+
+    pub fn solve(input: &str) -> isize {
+        let instructions = parse(input);
+        let mut registers = HashMap::new();
+        let mut max = 0;
+        for instruction in instructions {
+            if instruction.apply(&registers) {
+                *registers.entry(instruction.dest.clone()).or_insert(0) += instruction.delta;
+                max = max.max(*registers.get(&instruction.dest).unwrap());
+            }
+        }
+        max
+    }
+}
+
+pub fn main(test: bool) {
+    let test_input = "b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10"
+        .to_owned();
+    let puzzle_input = if test {
+        test_input
+    } else {
+        read_to_string("inputs/day_08_input.txt").unwrap()
+    };
+    let start = Instant::now();
+    println!("{}", part1::solve(&puzzle_input));
+    println!("Run in {:?}", start.elapsed());
+    let start = Instant::now();
+    println!("{}", part2::solve(&puzzle_input));
+    println!("Run in {:?}", start.elapsed());
+}
---
+++ 2017/day_09.rs
@@ -0,0 +1,86 @@
+//! https://adventofcode.com/2017/day/9
+//! https://adventofcode.com/2017/day/9/input
+
+use std::{fs::read_to_string, time::Instant};
+
+struct Group {
+    internal: Vec<Group>,
+    garbage: usize,
+}
+
+impl From<&str> for Group {
+    fn from(string: &str) -> Self {
+        fn recursive(string: &Vec<char>, mut i: usize) -> (Group, usize) {
+            let mut internal = Vec::new();
+            let mut is_garbage = false;
+            let mut garbage = 0;
+            while i < string.len() {
+                match string[i] {
+                    '{' if !is_garbage => {
+                        let (group, ni) = recursive(string, i + 1);
+                        i = ni;
+                        internal.push(group);
+                    }
+                    '}' if !is_garbage => return (Group { internal, garbage }, i),
+                    '<' if !is_garbage => is_garbage = true,
+                    '!' if is_garbage => i += 1,
+                    '>' if is_garbage => is_garbage = false,
+                    _ if is_garbage => garbage += 1,
+                    _ => {}
+                }
+                i += 1;
+            }
+            unreachable!(r#"Probably a "}}" is missing"#)
+        }
+        recursive(&string.chars().skip(1).collect(), 0).0
+    }
+}
+
+fn parse(input: &str) -> Group {
+    Group::from(input)
+}
+
+pub mod part1 {
+    use crate::day_09::{parse, Group};
+
+    fn score(group: &Group, partial: usize) -> usize {
+        let mut total = partial;
+        for internal in &group.internal {
+            total += score(internal, partial + 1);
+        }
+        total
+    }
+
+    pub fn solve(input: &str) -> usize {
+        let group = parse(input);
+        score(&group, 1)
+    }
+}
+
+pub mod part2 {
+    use crate::day_09::{parse, Group};
+
+    fn count_garbage(group: &Group) -> usize {
+        group.garbage + group.internal.iter().map(count_garbage).sum::<usize>()
+    }
+
+    pub fn solve(input: &str) -> usize {
+        let group = parse(input);
+        count_garbage(&group)
+    }
+}
+
+pub fn main(test: bool) {
+    let test_input = r#"{<!!>}"#.to_owned();
+    let puzzle_input = if test {
+        test_input
+    } else {
+        read_to_string("inputs/day_09_input.txt").unwrap()
+    };
+    let start = Instant::now();
+    println!("{}", part1::solve(&puzzle_input));
+    println!("Run in {:?}", start.elapsed());
+    let start = Instant::now();
+    println!("{}", part2::solve(&puzzle_input));
+    println!("Run in {:?}", start.elapsed());
+}
