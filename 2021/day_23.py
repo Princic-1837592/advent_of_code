79f287a88e9f9af5d0cd570bdd24eaf391df7646
Princic-1837592 <andreapri.99@gmail.com>
2023-04-26 19:21:48 +0200

    adjacency matrix is now part of state

--- 2021/day_23.py
+++ 2021/day_23.py
@@ -5,12 +5,13 @@ from collections import deque
 
 
 class State:
-    def __init__(self, positions: int, amphipods: int):
+    def __init__(self, positions: int, amphipods: int, adjacency: List[List[Tuple[int, int]]]):
         self.positions = [-1] * positions
         self.amphipods = [-1] * amphipods
+        self.adjacency = adjacency
 
     def __str__(self):
-        return f"State({self.positions}, {self.amphipods})"
+        return f"State({self.positions}, {self.amphipods}, {self.adjacency})"
 
 
 NEIGHBORS = ((0, 1), (0, -1), (1, 0), (-1, 0))
@@ -20,21 +21,22 @@ def parse(data: str) -> State:
     lines = data.splitlines()
     depth = len(lines) - 3
     positions = 7 + depth * 4
-    state = State(positions, depth * 4)
-    adjacency = [
-        [(1, 1)],
-        [(0, 1), (2, 2)],
-        [(1, 2), (3, 2)],
-        [(2, 2), (4, 2)],
-        [(3, 2), (5, 2)],
-        [(4, 2), (6, 1)],
-        [(5, 1)],
-    ]
+    state = State(
+        positions, depth * 4, [
+            [(1, 1)],
+            [(0, 1), (2, 2)],
+            [(1, 2), (3, 2)],
+            [(2, 2), (4, 2)],
+            [(3, 2), (5, 2)],
+            [(4, 2), (6, 1)],
+            [(5, 1)],
+        ]
+    )
     for room in range(4):
         room_index = room * depth + 7
-        adjacency.append([(room + 1, 2), (room + 2, 2)])
-        adjacency[room + 1].append((room_index, 2))
-        adjacency[room + 2].append((room_index, 2))
+        state.adjacency.append([(room + 1, 2), (room + 2, 2)])
+        state.adjacency[room + 1].append((room_index, 2))
+        state.adjacency[room + 2].append((room_index, 2))
         for row in range(depth):
             position_index = room_index + row
             input_i = row + 2
@@ -46,8 +48,8 @@ def parse(data: str) -> State:
                 amphipod_index += 1
             state.amphipods[amphipod_index] = position_index
             if row > 0:
-                adjacency.append([(position_index - 1, 1)])
-                adjacency[position_index - 1].append((position_index, 1))
+                state.adjacency.append([(position_index - 1, 1)])
+                state.adjacency[position_index - 1].append((position_index, 1))
     return state
 
 
