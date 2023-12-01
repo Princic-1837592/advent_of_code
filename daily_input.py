df83f859b773b5d7f3a071f47ad3f7d4b6aa033f
Princic-1837592 <andreapri.99@gmail.com>
2023-12-01 13:42:57 +0100

    daily input download

---
+++ daily_input.py
@@ -0,0 +1,31 @@
+from configparser import ConfigParser
+from datetime import date
+
+from requests import get
+
+today = date.today()
+if today.month != 12:
+    print("It's not December!")
+    exit(1)
+if today.day > 25:
+    print("This year's Advent of Code is over!")
+    exit(1)
+
+config = ConfigParser()
+config.read("local_config.ini")
+session = config["auto_download"]["session"]
+with open(f"{today.year}/inputs/day_{today.day:0>2}_input.txt", "w", encoding="utf-8") as file:
+    resp = get(
+        f"https://adventofcode.com/{today.year}/day/{today.day}/input",
+        cookies={"session": session},
+        headers={
+            "User-Agent": "https://github.com/Princic-1837592/advent_of_code/blob/main/setup.py"
+                          " by princic.1837592@studenti.uniroma1.it"
+        },
+    )
+    if resp.status_code != 200:
+        print("Something went wrong with the request!")
+        print(F"Error code: {resp.status_code}")
+        print(resp.text)
+        exit(1)
+    file.write(resp.text)
