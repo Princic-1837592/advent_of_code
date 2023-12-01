8e51a96b9240d3103f392589fb1e18f7e60c2d0b
Princic-1837592 <andreapri.99@gmail.com>
2023-12-01 13:28:22 +0100

    empty 2023

---
+++ 2023/day_19.rs
@@ -0,0 +1,41 @@
+//! https://adventofcode.com/2023/day/19
+//! https://adventofcode.com/2023/day/19/input
+
+#![allow(unused)]
+
+use std::{fs::read_to_string, time::Instant};
+
+fn parse(input: &str) -> usize {
+    0
+}
+
+pub mod part1 {
+    use crate::day_19::parse;
+
+    pub fn solve(input: &str) -> usize {
+        0
+    }
+}
+
+pub mod part2 {
+    use crate::day_19::parse;
+
+    pub fn solve(input: &str) -> usize {
+        0
+    }
+}
+
+pub fn main(test: bool) {
+    let test_input = "".to_owned();
+    let puzzle_input = if test {
+        test_input
+    } else {
+        read_to_string("inputs/day_19_input.txt").unwrap()
+    };
+    let start = Instant::now();
+    println!("{}", part1::solve(&puzzle_input));
+    println!("Run in {:?}", start.elapsed());
+    let start = Instant::now();
+    println!("{}", part2::solve(&puzzle_input));
+    println!("Run in {:?}", start.elapsed());
+}
