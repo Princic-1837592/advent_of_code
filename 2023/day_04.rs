e19c0763394c643ddcad27cc6a3efeb28bb043a6
Princic-1837592 <andreapri.99@gmail.com>
2023-12-04 08:18:48 +0100

    day 4

--- 2023/day_04.rs
+++ 2023/day_04.rs
@@ -1,19 +1,56 @@
 //! https://adventofcode.com/2023/day/4
 //! https://adventofcode.com/2023/day/4/input
 
-#![allow(unused)]
-
 use std::{fs::read_to_string, time::Instant};
 
-fn parse(input: &str) -> usize {
-    0
+#[derive(Copy, Clone, Debug, Default)]
+struct Card {
+    left: [usize; 10],
+    right: [usize; 25],
+}
+
+impl From<&str> for Card {
+    fn from(value: &str) -> Self {
+        let mut result = Card::default();
+        let mut parts = value.split('|');
+        let (left, right) = (parts.next().unwrap(), parts.next().unwrap());
+        for (i, n) in left.split_whitespace().skip(2).enumerate() {
+            result.left[i] = n.parse().unwrap();
+        }
+        for (i, n) in right.split_whitespace().enumerate() {
+            result.right[i] = n.parse().unwrap();
+        }
+        result.left.sort();
+        result.right.sort();
+        result
+    }
+}
+
+impl Card {
+    fn eval(&self) -> usize {
+        let count = self
+            .right
+            .iter()
+            .filter(|&&n| n > 0 && self.left.binary_search(&n).is_ok())
+            .count();
+        if count > 0 {
+            2_usize.pow(count as u32 - 1)
+        } else {
+            0
+        }
+    }
+}
+
+fn parse(input: &str) -> Vec<Card> {
+    input.lines().map(Card::from).collect()
 }
 
 pub mod part1 {
-    use crate::day_04::parse;
+    use crate::day_04::{parse, Card};
 
     pub fn solve(input: &str) -> usize {
-        0
+        let cards = parse(input);
+        cards.iter().map(Card::eval).sum()
     }
 }
 
@@ -21,12 +58,31 @@ pub mod part2 {
     use crate::day_04::parse;
 
     pub fn solve(input: &str) -> usize {
-        0
+        let mut cards: Vec<_> = parse(input).into_iter().map(|c| (1, c)).collect();
+        for c in 0..cards.len() {
+            let (n, card) = cards[c];
+            let points = card.eval();
+            let matches = if points == 0 {
+                0
+            } else {
+                points.ilog2() as usize + 1
+            };
+            for next in &mut cards[c + 1..c + 1 + matches] {
+                next.0 += n
+            }
+        }
+        cards.iter().map(|(n, _)| n).sum()
     }
 }
 
 pub fn main(test: bool) {
-    let test_input = "".to_owned();
+    let test_input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+        .to_owned();
     let puzzle_input = if test {
         test_input
     } else {
