f817c9cc0537070789582984ed06964ca2e4b7c8
Princic-1837592 <andreapri.99@gmail.com>
2023-12-05 07:42:53 +0100

    info about successful download

--- daily_input.py
+++ daily_input.py
@@ -14,18 +14,19 @@ if today.day > 25:
 config = ConfigParser()
 config.read("local_config.ini")
 session = config["auto_download"]["session"]
+resp = get(
+    f"https://adventofcode.com/{today.year}/day/{today.day}/input",
+    cookies={"session": session},
+    headers={
+        "User-Agent": "https://github.com/Princic-1837592/advent_of_code/blob/main/setup.py"
+                      " by princic.1837592@studenti.uniroma1.it"
+    },
+)
+if resp.status_code != 200:
+    print("Something went wrong with the request!")
+    print(F"Error code: {resp.status_code}")
+    print(resp.text)
+    exit(1)
 with open(f"{today.year}/inputs/day_{today.day:0>2}_input.txt", "w", encoding="utf-8") as file:
-    resp = get(
-        f"https://adventofcode.com/{today.year}/day/{today.day}/input",
-        cookies={"session": session},
-        headers={
-            "User-Agent": "https://github.com/Princic-1837592/advent_of_code/blob/main/setup.py"
-                          " by princic.1837592@studenti.uniroma1.it"
-        },
-    )
-    if resp.status_code != 200:
-        print("Something went wrong with the request!")
-        print(F"Error code: {resp.status_code}")
-        print(resp.text)
-        exit(1)
-    file.write(resp.text)
+    if file.write(resp.text) > 0:
+        print("Input downloaded successfully!")
