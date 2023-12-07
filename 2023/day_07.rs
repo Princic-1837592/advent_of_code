3b9a6b663fd3c9a1157154fac2830e822a0063f1
Princic-1837592 <andreapri.99@gmail.com>
2023-12-07 11:23:39 +0100

    day 7

--- 2023/day_07.rs
+++ 2023/day_07.rs
@@ -6,274 +6,144 @@ use std::{
     time::{Duration, Instant},
 };
 
-pub mod part1 {
-    #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
-    enum Card {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-        Ace,
-    }
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+enum Card {
+    Joker,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
 
-    impl From<char> for Card {
-        fn from(value: char) -> Self {
-            match value {
-                '2' => Self::Two,
-                '3' => Self::Three,
-                '4' => Self::Four,
-                '5' => Self::Five,
-                '6' => Self::Six,
-                '7' => Self::Seven,
-                '8' => Self::Eight,
-                '9' => Self::Nine,
-                'T' => Self::Ten,
-                'J' => Self::Jack,
-                'Q' => Self::Queen,
-                'K' => Self::King,
-                'A' => Self::Ace,
-                _ => unreachable!(),
-            }
+impl From<char> for Card {
+    fn from(value: char) -> Self {
+        match value {
+            '?' => Card::Joker,
+            '2' => Self::Two,
+            '3' => Self::Three,
+            '4' => Self::Four,
+            '5' => Self::Five,
+            '6' => Self::Six,
+            '7' => Self::Seven,
+            '8' => Self::Eight,
+            '9' => Self::Nine,
+            'T' => Self::Ten,
+            'J' => Self::Jack,
+            'Q' => Self::Queen,
+            'K' => Self::King,
+            'A' => Self::Ace,
+            _ => unreachable!(),
         }
-    }
-
-    #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
-    enum Type {
-        HighCard,
-        OnePair,
-        TwoPairs,
-        ThreeOfAKind,
-        FullHouse,
-        FourOfAKind,
-        FiveOfAKind,
-    }
-
-    type Cards = [Card; 5];
-
-    #[derive(Copy, Clone, Debug)]
-    struct Hand {
-        cards: Cards,
-        strength: Type,
-    }
-
-    impl Hand {
-        fn new(cards: Cards) -> Self {
-            Self {
-                cards,
-                strength: Self::compute_score(cards),
-            }
-        }
-
-        fn compute_score(cards: Cards) -> Type {
-            let mut occurrencies = [0; 13];
-            for card in cards {
-                occurrencies[card as usize] += 1;
-            }
-            occurrencies.sort();
-            occurrencies.reverse();
-            match occurrencies {
-                [5, ..] => Type::FiveOfAKind,
-                [4, ..] => Type::FourOfAKind,
-                [3, 2, ..] => Type::FullHouse,
-                [3, 1, ..] => Type::ThreeOfAKind,
-                [2, 2, ..] => Type::TwoPairs,
-                [2, 1, ..] => Type::OnePair,
-                [1, ..] => Type::HighCard,
-                _ => {
-                    unreachable!()
-                }
-            }
-        }
-    }
-
-    impl From<&str> for Hand {
-        fn from(value: &str) -> Self {
-            let mut cards = [Card::Two; 5];
-            for (i, c) in value.chars().enumerate() {
-                cards[i] = Card::from(c);
-            }
-            Self::new(cards)
-        }
-    }
-
-    #[derive(Copy, Clone, Debug)]
-    pub struct Play {
-        hand: Hand,
-        bid: usize,
-    }
-
-    impl From<&str> for Play {
-        fn from(value: &str) -> Self {
-            let mut parts = value.split_whitespace();
-            Self {
-                hand: parts.next().unwrap().into(),
-                bid: parts.next().unwrap().parse().unwrap(),
-            }
-        }
-    }
-
-    type Parsed = Vec<Play>;
-
-    pub fn parse(input: &str) -> Parsed {
-        input.lines().map(Play::from).collect()
-    }
-
-    pub fn solve(mut plays: Parsed) -> usize {
-        plays.sort_by_key(|play| (play.hand.strength, play.hand.cards));
-        plays
-            .iter()
-            .enumerate()
-            .map(|(i, play)| (i + 1) * play.bid)
-            .sum()
     }
 }
 
-pub mod part2 {
-    #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
-    enum Card {
-        Joker,
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Queen,
-        King,
-        Ace,
-    }
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+enum Type {
+    HighCard,
+    OnePair,
+    TwoPairs,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
 
-    impl From<char> for Card {
-        fn from(value: char) -> Self {
-            match value {
-                'J' => Self::Joker,
-                '2' => Self::Two,
-                '3' => Self::Three,
-                '4' => Self::Four,
-                '5' => Self::Five,
-                '6' => Self::Six,
-                '7' => Self::Seven,
-                '8' => Self::Eight,
-                '9' => Self::Nine,
-                'T' => Self::Ten,
-                'Q' => Self::Queen,
-                'K' => Self::King,
-                'A' => Self::Ace,
-                _ => unreachable!(),
-            }
+type Cards = [Card; 5];
+
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct Hand {
+    strength: Type,
+    cards: Cards,
+}
+
+impl Hand {
+    fn new(cards: Cards) -> Self {
+        Self {
+            cards,
+            strength: Self::compute_score(cards),
         }
     }
 
-    #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
-    enum Type {
-        HighCard,
-        OnePair,
-        TwoPairs,
-        ThreeOfAKind,
-        FullHouse,
-        FourOfAKind,
-        FiveOfAKind,
-    }
-
-    type Cards = [Card; 5];
-
-    #[derive(Copy, Clone, Debug)]
-    struct Hand {
-        cards: Cards,
-        strength: Type,
-    }
-
-    impl Hand {
-        fn new(cards: Cards) -> Self {
-            Self {
-                cards,
-                strength: Self::compute_score(cards),
+    fn compute_score(cards: Cards) -> Type {
+        let mut occurrencies = [0; 14];
+        let mut jokers = 0;
+        for card in cards {
+            if let Card::Joker = card {
+                jokers += 1;
+            } else {
+                occurrencies[card as usize] += 1;
             }
         }
-
-        fn compute_score(cards: Cards) -> Type {
-            let mut occurrencies = [0; 13];
-            let mut jokers = 0;
-            for card in cards {
-                if let Card::Joker = card {
-                    jokers += 1;
-                } else {
-                    occurrencies[card as usize] += 1;
-                }
-            }
-            occurrencies.sort();
-            occurrencies.reverse();
-            match (jokers, occurrencies) {
-                (0, [5, ..])
-                | (1, [4, ..])
-                | (2, [3, ..])
-                | (3, [2, ..])
-                | (4, [1, ..])
-                | (5, [0, ..]) => Type::FiveOfAKind,
-                (0, [4, ..]) | (1, [3, ..]) | (2, [2, ..]) | (3, [1, ..]) => Type::FourOfAKind,
-                (0, [3, 2, ..]) | (1, [2, 2, ..]) => Type::FullHouse,
-                (0, [3, 1, ..]) | (1, [2, 1, ..]) | (2, [1, ..]) => Type::ThreeOfAKind,
-                (0, [2, 2, ..]) => Type::TwoPairs,
-                (0, [2, 1, ..]) | (1, [1, ..]) => Type::OnePair,
-                (0, [1, ..]) => Type::HighCard,
-                _ => {
-                    unreachable!()
-                }
+        occurrencies.sort();
+        occurrencies.reverse();
+        match (jokers, occurrencies) {
+            (0, [5, ..])
+            | (1, [4, ..])
+            | (2, [3, ..])
+            | (3, [2, ..])
+            | (4, [1, ..])
+            | (5, [0, ..]) => Type::FiveOfAKind,
+            (0, [4, ..]) | (1, [3, ..]) | (2, [2, ..]) | (3, [1, ..]) => Type::FourOfAKind,
+            (0, [3, 2, ..]) | (1, [2, 2, ..]) => Type::FullHouse,
+            (0, [3, 1, ..]) | (1, [2, 1, ..]) | (2, [1, ..]) => Type::ThreeOfAKind,
+            (0, [2, 2, ..]) => Type::TwoPairs,
+            (0, [2, 1, ..]) | (1, [1, ..]) => Type::OnePair,
+            (0, [1, ..]) => Type::HighCard,
+            _ => {
+                unreachable!()
             }
         }
     }
+}
 
-    impl From<&str> for Hand {
-        fn from(value: &str) -> Self {
-            let mut cards = [Card::Two; 5];
-            for (i, c) in value.chars().enumerate() {
-                cards[i] = Card::from(c);
-            }
-            Self::new(cards)
+impl From<&str> for Hand {
+    fn from(value: &str) -> Self {
+        let mut cards = [Card::Two; 5];
+        for (i, c) in value.chars().enumerate() {
+            cards[i] = Card::from(c);
+        }
+        Self::new(cards)
+    }
+}
+
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub struct Play {
+    hand: Hand,
+    bid: usize,
+}
+
+impl From<&str> for Play {
+    fn from(value: &str) -> Self {
+        let mut parts = value.split_whitespace();
+        Self {
+            hand: parts.next().unwrap().into(),
+            bid: parts.next().unwrap().parse().unwrap(),
         }
     }
+}
 
-    #[derive(Copy, Clone, Debug)]
-    pub struct Play {
-        hand: Hand,
-        bid: usize,
-    }
+type Parsed = Vec<Play>;
 
-    impl From<&str> for Play {
-        fn from(value: &str) -> Self {
-            let mut parts = value.split_whitespace();
-            Self {
-                hand: parts.next().unwrap().into(),
-                bid: parts.next().unwrap().parse().unwrap(),
-            }
-        }
-    }
+pub fn parse(input: &str) -> Parsed {
+    input.lines().map(Play::from).collect()
+}
 
-    type Parsed = Vec<Play>;
-
-    pub fn parse(input: &str) -> Parsed {
-        input.lines().map(Play::from).collect()
-    }
-
-    pub fn solve(mut plays: Parsed) -> usize {
-        plays.sort_by_key(|play| (play.hand.strength, play.hand.cards));
-        plays
-            .iter()
-            .enumerate()
-            .map(|(i, play)| (i + 1) * play.bid)
-            .sum()
-    }
+fn solve(mut plays: Parsed) -> usize {
+    plays.sort();
+    plays
+        .iter()
+        .enumerate()
+        .map(|(i, play)| (i + 1) * play.bid)
+        .sum()
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
@@ -292,7 +162,7 @@ QQQJA 483"
     let mut total = Duration::default();
 
     let start = Instant::now();
-    let parsed = part1::parse(&puzzle_input);
+    let parsed = parse(&puzzle_input);
     let elapsed = start.elapsed();
     if verbose {
         println!("Parsed in {:?}", elapsed);
@@ -300,14 +170,14 @@ QQQJA 483"
     }
 
     let start = Instant::now();
-    let result = part1::solve(parsed.clone());
+    let result = solve(parsed.clone());
     let elapsed = start.elapsed();
     println!("{}", result);
     println!("First part in {:?}", elapsed);
     total += elapsed;
 
     let start = Instant::now();
-    let parsed = part2::parse(&puzzle_input);
+    let parsed = parse(&puzzle_input.replace('J', "?"));
     let elapsed = start.elapsed();
     if verbose {
         println!("Parsed in {:?}", elapsed);
@@ -315,7 +185,7 @@ QQQJA 483"
     }
 
     let start = Instant::now();
-    let result = part2::solve(parsed);
+    let result = solve(parsed);
     let elapsed = start.elapsed();
     println!("{}", result);
     println!("Second part in {:?}", elapsed);
