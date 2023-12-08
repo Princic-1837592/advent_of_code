b95a3dfcb383ff2207b4924a8b527c7c7fdc93f3
Princic-1837592 <andreapri.99@gmail.com>
2023-12-08 10:35:54 +0100

    day 8

--- 2023/day_08.rs
+++ 2023/day_08.rs
@@ -6,30 +6,108 @@ use std::{
     time::{Duration, Instant},
 };
 
-type Parsed = Vec<usize>;
+type Node = [usize; 2];
 
-fn parse(_input: &str) -> Parsed {
-    vec![]
+type Parsed = (Vec<usize>, Vec<Node>);
+
+fn parse(input: &str) -> Parsed {
+    let mut lines = input.lines();
+    let instructions = lines
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| if c == 'L' { 0 } else { 1 })
+        .collect();
+    let mut nodes = vec![[usize::MAX, usize::MAX]];
+    for line in lines.skip(1) {
+        let mut chars = line.chars();
+        let node = (chars.next().unwrap() as usize - 'A' as usize) * 26 * 26
+            + (chars.next().unwrap() as usize - 'A' as usize) * 26
+            + (chars.next().unwrap() as usize - 'A' as usize);
+        let mut chars = chars.skip(4);
+        let left = (chars.next().unwrap() as usize - 'A' as usize) * 26 * 26
+            + (chars.next().unwrap() as usize - 'A' as usize) * 26
+            + (chars.next().unwrap() as usize - 'A' as usize);
+        let mut chars = chars.skip(2);
+        let right = (chars.next().unwrap() as usize - 'A' as usize) * 26 * 26
+            + (chars.next().unwrap() as usize - 'A' as usize) * 26
+            + (chars.next().unwrap() as usize - 'A' as usize);
+        if nodes.len() <= node {
+            nodes.extend(vec![[usize::MAX, usize::MAX]; node - nodes.len() + 1]);
+        }
+        nodes[node][0] = left;
+        nodes[node][1] = right;
+    }
+    (instructions, nodes)
 }
 
 pub mod part1 {
     use super::Parsed;
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    pub fn solve((instructions, graph): Parsed) -> usize {
+        let mut node = 0;
+        for i in 0.. {
+            if node == 25 * 26 * 26 + 25 * 26 + 25 {
+                return i;
+            }
+            node = graph[node][instructions[i % instructions.len()]];
+        }
+        unreachable!()
     }
 }
 
 pub mod part2 {
     use super::Parsed;
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    fn gcd(a: usize, b: usize) -> usize {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+
+    fn lcm(a: usize, b: usize) -> usize {
+        if a > b {
+            (a / gcd(a, b)) * b
+        } else {
+            (b / gcd(a, b)) * a
+        }
+    }
+
+    pub fn solve((instructions, graph): Parsed) -> usize {
+        let nodes: Vec<_> = graph
+            .iter()
+            .enumerate()
+            .filter_map(|(n, d)| (n % 26 == 0 && d[0] != usize::MAX).then_some(n))
+            .collect();
+        let cycles: Vec<_> = nodes
+            .into_iter()
+            .map(|mut node| {
+                for i in 0.. {
+                    if node % 26 == 25 {
+                        return i;
+                    }
+                    node = graph[node][instructions[i % instructions.len()]];
+                }
+                unreachable!()
+            })
+            .collect();
+        cycles.into_iter().fold(1, lcm)
     }
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-    let test_input = "".to_owned();
+    let test_input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"
+        .to_owned();
     let puzzle_input = if test {
         test_input
     } else {
