43f560b95cf59b3552312f97f261484a5b616ed6
Princic-1837592 <andreapri.99@gmail.com>
2023-12-09 10:32:50 +0100

    day 9

--- 2023/day_09.rs
+++ 2023/day_09.rs
@@ -6,30 +6,79 @@ use std::{
     time::{Duration, Instant},
 };
 
-type Parsed = Vec<usize>;
+type History = Vec<isize>;
 
-fn parse(_input: &str) -> Parsed {
-    vec![]
+type Parsed = Vec<History>;
+
+fn parse(input: &str) -> Parsed {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
 pub mod part1 {
-    use super::Parsed;
+    use super::{History, Parsed};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    fn reduce(mut history: History) -> isize {
+        let mut result = 0;
+        let mut all_zero = false;
+        while !all_zero {
+            all_zero = true;
+            for i in 0..history.len() - 1 {
+                let diff = history[i + 1] - history[i];
+                if diff != 0 {
+                    all_zero = false;
+                }
+                history[i] = diff;
+            }
+            result += history.pop().unwrap();
+        }
+        result
+    }
+
+    pub fn solve(histories: Parsed) -> isize {
+        histories.into_iter().map(reduce).sum()
     }
 }
 
 pub mod part2 {
-    use super::Parsed;
+    use super::{History, Parsed};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    fn reduce(mut history: History) -> isize {
+        let mut result = 0;
+        let mut all_zero = false;
+        let mut sum = true;
+        while !all_zero {
+            all_zero = true;
+            result += if sum { history[0] } else { -history[0] };
+            sum = !sum;
+            for i in 0..history.len() - 1 {
+                let diff = history[i + 1] - history[i];
+                if diff != 0 {
+                    all_zero = false;
+                }
+                history[i] = diff;
+            }
+            history.pop();
+        }
+        result
+    }
+
+    pub fn solve(histories: Parsed) -> isize {
+        histories.into_iter().map(reduce).sum()
     }
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-    let test_input = "".to_owned();
+    let test_input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
+        .to_owned();
     let puzzle_input = if test {
         test_input
     } else {
