94bfb60fb13f4b137c8199e9b353a921b8cf7d3d
Princic-1837592 <andreapri.99@gmail.com>
2023-12-10 12:28:17 +0100

    day 10 part 1

--- 2023/day_10.rs
+++ 2023/day_10.rs
@@ -6,17 +6,173 @@ use std::{
     time::{Duration, Instant},
 };
 
-type Parsed = Vec<usize>;
+#[derive(Copy, Clone, Debug)]
+pub enum Pipe {
+    NS,    // |
+    EW,    // -
+    NE,    // L
+    NW,    // J
+    SW,    // 7
+    SE,    // F
+    Empty, // .
+    Start, // S
+}
 
-fn parse(_input: &str) -> Parsed {
-    vec![]
+impl Pipe {
+    fn open_west(&self) -> bool {
+        match self {
+            Pipe::NS | Pipe::NE | Pipe::SE | Pipe::Empty => false,
+            Pipe::EW | Pipe::SW | Pipe::NW | Pipe::Start => true,
+        }
+    }
+
+    fn open_east(&self) -> bool {
+        match self {
+            Pipe::EW | Pipe::NE | Pipe::SE | Pipe::Start => true,
+            Pipe::NS | Pipe::SW | Pipe::NW | Pipe::Empty => false,
+        }
+    }
+
+    fn open_north(&self) -> bool {
+        match self {
+            Pipe::NS | Pipe::NE | Pipe::NW | Pipe::Start => true,
+            Pipe::EW | Pipe::SW | Pipe::SE | Pipe::Empty => false,
+        }
+    }
+
+    fn open_south(&self) -> bool {
+        match self {
+            Pipe::EW | Pipe::NE | Pipe::NW | Pipe::Empty => false,
+            Pipe::NS | Pipe::SW | Pipe::SE | Pipe::Start => true,
+        }
+    }
+
+    fn open(&self, direction: Direction) -> bool {
+        match direction {
+            Direction::N => self.open_north(),
+            Direction::E => self.open_east(),
+            Direction::S => self.open_south(),
+            Direction::W => self.open_west(),
+        }
+    }
+
+    fn other(&self, direction: Direction) -> Direction {
+        match (self, direction) {
+            (Pipe::NS, Direction::N) => Direction::S,
+            (Pipe::NS, Direction::S) => Direction::N,
+            (Pipe::EW, Direction::W) => Direction::E,
+            (Pipe::EW, Direction::E) => Direction::W,
+            (Pipe::NE, Direction::E) => Direction::N,
+            (Pipe::NE, Direction::N) => Direction::E,
+            (Pipe::NW, Direction::N) => Direction::W,
+            (Pipe::NW, Direction::W) => Direction::N,
+            (Pipe::SW, Direction::W) => Direction::S,
+            (Pipe::SW, Direction::S) => Direction::W,
+            (Pipe::SE, Direction::E) => Direction::S,
+            (Pipe::SE, Direction::S) => Direction::E,
+            (Pipe::Empty, _) => unreachable!(),
+            (Pipe::Start, _) => Direction::N,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<char> for Pipe {
+    fn from(value: char) -> Self {
+        match value {
+            '|' => Self::NS,
+            '-' => Self::EW,
+            'L' => Self::NE,
+            'J' => Self::NW,
+            '7' => Self::SW,
+            'F' => Self::SE,
+            'S' => Self::Start,
+            '.' => Self::Empty,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Direction {
+    N,
+    E,
+    S,
+    W,
+}
+
+impl Direction {
+    fn transform(&self, i: usize, j: usize) -> Option<Coord> {
+        match self {
+            Direction::N => i.checked_sub(1).map(|i| (i, j)),
+            Direction::E => Some((i, j + 1)),
+            Direction::S => Some((i + 1, j)),
+            Direction::W => j.checked_sub(1).map(|j| (i, j)),
+        }
+    }
+
+    fn reverse(&self) -> Direction {
+        match self {
+            Direction::N => Direction::S,
+            Direction::E => Direction::W,
+            Direction::S => Direction::N,
+            Direction::W => Direction::E,
+        }
+    }
+}
+
+const DIRECTIONS: [Direction; 4] = [Direction::S, Direction::N, Direction::W, Direction::E];
+
+type Pipes = Vec<Vec<Pipe>>;
+
+type Coord = (usize, usize);
+
+type Parsed = (Pipes, Coord);
+
+fn parse(input: &str) -> Parsed {
+    let result: Vec<Vec<_>> = input
+        .lines()
+        .map(|line| line.chars().map(Pipe::from).collect())
+        .collect();
+    for (i, row) in result.iter().enumerate() {
+        for (j, pipe) in row.iter().enumerate() {
+            if let Pipe::Start = pipe {
+                return (result, (i, j));
+            }
+        }
+    }
+    unreachable!()
 }
 
 pub mod part1 {
-    use super::Parsed;
+    use super::{Direction, Parsed, Pipes, DIRECTIONS};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    fn explore(pipes: &Pipes, oi: usize, oj: usize, mut direction: Direction) -> Option<usize> {
+        let (mut i, mut j) = (oi, oj);
+        let mut length = 0;
+        while (i, j) != (oi, oj) || length == 0 {
+            let (ni, nj) = match direction.transform(i, j) {
+                None => return None,
+                Some(n) => n,
+            };
+            let nd = direction.reverse();
+            if !pipes[ni][nj].open(nd) {
+                return None;
+            }
+            (i, j) = (ni, nj);
+            direction = pipes[i][j].other(nd);
+            length += 1;
+        }
+        Some(length)
+    }
+
+    pub fn solve((pipes, (i, j)): Parsed) -> usize {
+        for direction in DIRECTIONS {
+            if let Some(length) = explore(&pipes, i, j, direction) {
+                return length / 2;
+            }
+        }
+        unreachable!()
     }
 }
 
