4ff5377f04fd60f7bfd3fc70d98af17efbb95094
Princic-1837592 <andreapri.99@gmail.com>
2023-12-13 09:08:28 +0100

    day 12
    dynamic programming formula taken from https://www.reddit.com/r/adventofcode/comments/18ge41g/comment/kd18cl9

--- 2023/day_12.rs
+++ 2023/day_12.rs
@@ -6,30 +6,131 @@ use std::{
     time::{Duration, Instant},
 };
 
-type Parsed = Vec<usize>;
+#[derive(Copy, Clone, Debug)]
+enum Spring {
+    Operational,
+    Damaged,
+    Unknown,
+}
 
-fn parse(_input: &str) -> Parsed {
-    vec![]
+impl From<char> for Spring {
+    fn from(char: char) -> Self {
+        match char {
+            '.' => Self::Operational,
+            '?' => Self::Unknown,
+            '#' => Self::Damaged,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Group {
+    springs: Vec<Spring>,
+    sizes: Vec<u8>,
+}
+
+impl From<&str> for Group {
+    fn from(value: &str) -> Self {
+        let mut parts = value.split_whitespace();
+        let springs = parts.next().unwrap().chars().map(Spring::from).collect();
+        let sizes = parts
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|n| n.parse().unwrap())
+            .collect();
+        Self { springs, sizes }
+    }
+}
+
+type Parsed = Vec<Group>;
+
+fn parse(input: &str) -> Parsed {
+    input.lines().map(Group::from).collect()
+}
+
+// https://www.reddit.com/r/adventofcode/comments/18ge41g/comment/kd18cl9
+fn dynamic(springs: &[Spring], damaged_group: &[bool]) -> usize {
+    let (n, m) = (springs.len(), damaged_group.len());
+    let mut dp = vec![vec![0; m + 1]; n + 1];
+    dp[n][m] = 1;
+    for i in (0..=n - 1).rev() {
+        let spring = springs[i];
+        for j in (0..=m - 1).rev() {
+            dp[i][j] = match (spring, damaged_group[j]) {
+                (Spring::Operational | Spring::Unknown, false) => dp[i + 1][j + 1] + dp[i + 1][j],
+                (Spring::Damaged | Spring::Unknown, true) => dp[i + 1][j + 1],
+                _ => 0,
+            };
+        }
+    }
+    dp[0][0]
 }
 
 pub mod part1 {
-    use super::Parsed;
+    use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    use super::{dynamic, Group, Parsed, Spring};
+
+    fn arrangements(Group { mut springs, sizes }: Group) -> usize {
+        let mut damaged =
+            Vec::with_capacity(sizes.len() + sizes.iter().cloned().sum::<u8>() as usize + 2);
+        damaged.push(false);
+        for size in sizes.iter().copied() {
+            damaged.resize(damaged.len() + size as usize, true);
+            damaged.push(false);
+        }
+        springs.insert(0, Spring::Operational);
+        springs.push(Spring::Operational);
+        dynamic(&springs, &damaged)
+    }
+
+    pub fn solve(groups: Parsed) -> usize {
+        groups.into_par_iter().map(arrangements).sum()
     }
 }
 
 pub mod part2 {
-    use super::Parsed;
+    use std::iter::repeat;
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+
+    use super::{dynamic, Group, Parsed, Spring};
+
+    fn arrangements(Group { mut springs, sizes }: Group) -> usize {
+        let mut damaged =
+            Vec::with_capacity((sizes.len() + sizes.iter().cloned().sum::<u8>() as usize) * 5 + 2);
+        damaged.push(false);
+        for size in [sizes]
+            .into_iter()
+            .flat_map(|n| repeat(n).take(5))
+            .flatten()
+        {
+            damaged.resize(damaged.len() + size as usize, true);
+            damaged.push(false);
+        }
+        springs.push(Spring::Unknown);
+        springs = repeat(springs).take(5).flatten().collect();
+        springs.pop();
+        springs.insert(0, Spring::Operational);
+        springs.push(Spring::Operational);
+        dynamic(&springs, &damaged)
+    }
+
+    pub fn solve(groups: Parsed) -> usize {
+        groups.into_par_iter().map(arrangements).sum()
     }
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-    let test_input = "".to_owned();
+    let test_input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"
+        .to_owned();
     let puzzle_input = if test {
         test_input
     } else {
