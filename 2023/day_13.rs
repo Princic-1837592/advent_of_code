f2fc55485fd1e62efacec88a3f87471c7f6b5749
Princic-1837592 <andreapri.99@gmail.com>
2023-12-14 10:02:11 +0100

    day 13

--- 2023/day_13.rs
+++ 2023/day_13.rs
@@ -6,30 +6,122 @@ use std::{
     time::{Duration, Instant},
 };
 
-type Parsed = Vec<usize>;
+use utils::matrix::rotate_left;
 
-fn parse(_input: &str) -> Parsed {
-    vec![]
+type Pattern = Vec<Vec<bool>>;
+
+type Parsed = Vec<Pattern>;
+
+fn parse(input: &str) -> Parsed {
+    let separator = "\n".repeat(2);
+    input
+        .replace("\r\n", "\n")
+        .split(&separator)
+        .map(|pattern| {
+            pattern
+                .lines()
+                .map(|line| line.chars().map(|char| char == '#').collect())
+                .collect()
+        })
+        .collect()
+}
+
+fn find_vertical(pattern: &Pattern) -> Option<usize> {
+    'column: for c in 1..pattern[0].len() {
+        for row in pattern {
+            for dj in 0..c.min(pattern[0].len() - c) {
+                if row[c - dj - 1] != row[c + dj] {
+                    continue 'column;
+                }
+            }
+        }
+        return Some(c);
+    }
+    None
+}
+
+fn find_reflection(pattern: &Pattern) -> usize {
+    find_vertical(pattern).unwrap_or_else(|| {
+        let pattern = rotate_left(pattern);
+        find_vertical(&pattern).unwrap() * 100
+    })
 }
 
 pub mod part1 {
-    use super::Parsed;
+    use super::{find_reflection, Parsed};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    pub fn solve(patterns: Parsed) -> usize {
+        patterns.iter().map(find_reflection).sum()
     }
 }
 
 pub mod part2 {
-    use super::Parsed;
+    use std::collections::HashSet;
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    use utils::matrix::rotate_left;
+
+    use super::{Parsed, Pattern};
+
+    fn find_verticals(pattern: &Pattern) -> HashSet<usize> {
+        let mut result = HashSet::new();
+        'column: for c in 1..pattern[0].len() {
+            for row in pattern {
+                for dj in 0..c.min(pattern[0].len() - c) {
+                    if row[c - dj - 1] != row[c + dj] {
+                        continue 'column;
+                    }
+                }
+            }
+            result.insert(c);
+        }
+        result
+    }
+
+    fn find_reflections(pattern: &Pattern) -> HashSet<usize> {
+        let mut result = find_verticals(pattern);
+        let pattern = rotate_left(pattern);
+        result.extend(find_verticals(&pattern).iter().map(|v| v * 100));
+        result
+    }
+
+    fn find_reflection(pattern: Pattern) -> usize {
+        let original = super::find_reflection(&pattern);
+        for (i, row) in pattern.iter().enumerate() {
+            for (j, rock) in row.iter().enumerate() {
+                let mut pattern = pattern.clone();
+                pattern[i][j] = !rock;
+                let mut new = find_reflections(&pattern);
+                new.remove(&original);
+                if let Some(&result) = new.iter().next() {
+                    return result;
+                }
+            }
+        }
+        original
+    }
+
+    pub fn solve(patterns: Parsed) -> usize {
+        patterns.into_iter().map(find_reflection).sum()
     }
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-    let test_input = "".to_owned();
+    let test_input = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"
+        .to_owned();
     let puzzle_input = if test {
         test_input
     } else {
