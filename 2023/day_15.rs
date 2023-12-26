d742f9d2fb84d730d4e26acc589a1d9c190dbe90
Princic-1837592 <andreapri.99@gmail.com>
2023-12-26 16:44:49 +0100

    day 15

--- 2023/day_15.rs
+++ 2023/day_15.rs
@@ -6,30 +6,80 @@ use std::{
     time::{Duration, Instant},
 };
 
-type Parsed = Vec<usize>;
+type Parsed<'a> = Vec<Vec<usize>>;
 
-fn parse(_input: &str) -> Parsed {
-    vec![]
+fn parse(input: &str) -> Parsed {
+    input
+        .trim()
+        .split(',')
+        .map(|part| part.chars().map(|char| char as usize).collect())
+        .collect()
+}
+
+fn hash(chars: &[usize]) -> usize {
+    chars.iter().fold(0, |acc, char| ((acc + char) * 17) % 256)
 }
 
 pub mod part1 {
-    use super::Parsed;
+    use super::{hash, Parsed};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    pub fn solve(parsed: Parsed) -> usize {
+        parsed.iter().map(|part| hash(part)).sum()
     }
 }
 
 pub mod part2 {
-    use super::Parsed;
+    use super::{hash, Parsed};
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
+    #[derive(Copy, Clone, Debug)]
+    struct Lens {
+        id: usize,
+        focal_length: usize,
+    }
+
+    fn compute_id(lens: &[usize]) -> usize {
+        lens.iter()
+            .zip(0..)
+            .map(|(&c, e)| c * 26_usize.pow(e))
+            .sum()
+    }
+
+    pub fn solve(steps: Parsed) -> usize {
+        let mut boxes = vec![Vec::<Lens>::new(); 256];
+        for step in steps {
+            if step[step.len() - 1] == '-' as usize {
+                let hash = hash(&step[..step.len() - 1]);
+                let id = compute_id(&step[..step.len() - 1]);
+                boxes[hash].retain(|lens| lens.id != id);
+            } else {
+                let hash = hash(&step[..step.len() - 2]);
+                let id = compute_id(&step[..step.len() - 2]);
+                let number = step[step.len() - 1] - '0' as usize;
+                let lens = Lens {
+                    id,
+                    focal_length: number,
+                };
+                if let Some(l) = boxes[hash].iter().position(|lens| lens.id == id) {
+                    boxes[hash][l].focal_length = number;
+                } else {
+                    boxes[hash].push(lens);
+                }
+            }
+        }
+        boxes
+            .into_iter()
+            .enumerate()
+            .flat_map(|(h, hash)| {
+                hash.into_iter()
+                    .enumerate()
+                    .map(move |(i, lens)| (h + 1) * (i + 1) * lens.focal_length)
+            })
+            .sum()
     }
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-    let test_input = "".to_owned();
+    let test_input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".to_owned();
     let puzzle_input = if test {
         test_input
     } else {
