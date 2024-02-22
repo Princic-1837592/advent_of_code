57bd11319cbe6f0ea43cd52ace66f5558850820a
Princic-1837592 <andreapri.99@gmail.com>
2024-02-23 00:40:51 +0100

    fixed `NEAR`

--- utils/src/coords.rs
+++ utils/src/coords.rs
@@ -4,23 +4,23 @@ pub const NEAR: [(isize, isize); 8] = [
     (-1, 0),
     (-1, 1),
     (0, 1),
-    (1, 0),
     (0, -1),
-    (0, 0),
-    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
 ];
 
-fn iter(near: &[(isize, isize)], i: isize, j: isize) -> impl Iterator<Item = (isize, isize)> + '_ {
+fn iter(near: &[(isize, isize)], i: isize, j: isize) -> impl Iterator<Item=(isize, isize)> + '_ {
     near.iter().filter_map(move |(ni, nj)| {
         i.checked_add(*ni)
             .and_then(|i| j.checked_add(*nj).map(|j| (i, j)))
     })
 }
 
-pub fn iter_cross_near(i: isize, j: isize) -> impl Iterator<Item = (isize, isize)> {
+pub fn iter_cross_near(i: isize, j: isize) -> impl Iterator<Item=(isize, isize)> {
     iter(&CROSS_NEAR, i, j)
 }
 
-pub fn iter_near(i: isize, j: isize) -> impl Iterator<Item = (isize, isize)> {
+pub fn iter_near(i: isize, j: isize) -> impl Iterator<Item=(isize, isize)> {
     iter(&NEAR, i, j)
 }
