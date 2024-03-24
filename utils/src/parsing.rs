1292e6af374c4b52191e691143f4928e5e6c374e
Princic-1837592 <andreapri.99@gmail.com>
2024-03-24 13:17:28 +0100

    generic functions

--- utils/src/parsing.rs
+++ utils/src/parsing.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Debug, str::FromStr};
+
 pub fn parse_alpha<const S: char>(chars: &str) -> usize {
     let mut result = 0;
     for char in chars.chars() {
@@ -19,6 +21,18 @@ macro_rules! parse_matrix {
 #[macro_export]
 macro_rules! parse_lines {
     ($input:ident, $t:ty) => {
-        $input.lines().map(<$t>::from).collect()
+        $input.lines().map(|l| l.parse().unwrap()).collect()
     };
+}
+
+pub fn parse_matrix<T: From<char>>(input: &str) -> Vec<Vec<T>> {
+    parse_matrix!(input, T)
+}
+
+pub fn parse_line<T>(input: &str) -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: Debug,
+{
+    parse_lines!(input, T)
 }
