64dd9620a4426bf2345c1d746db4ff4ad4c72672
Princic-1837592 <andreapri.99@gmail.com>
2024-03-26 19:26:36 +0100

    added `T: FromStr` to where clause

--- utils/proc_macros/src/structs/mod.rs
+++ utils/proc_macros/src/structs/mod.rs
@@ -1,7 +1,10 @@
 use deluxe::{extract_attributes, ExtractAttributes};
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{Data::Struct, DeriveInput, Expr, Fields::Named, FieldsNamed, Meta};
+use syn::{
+    parse_quote, punctuated::Punctuated, token::Comma, Data::Struct, DeriveInput, Expr, Field,
+    Fields::Named, FieldsNamed, GenericParam, Generics, Meta, TypeParam, WhereClause,
+};
 
 #[derive(ExtractAttributes)]
 #[deluxe(attributes(separator))]
@@ -11,43 +14,16 @@ pub(crate) fn from_line_derive_internal(item: TokenStream) -> deluxe::Result<Tok
     let mut ast: DeriveInput = syn::parse2(item)?;
     let separator: Result<Separator, _> = extract_attributes(&mut ast);
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+    let where_clause = add_where_clause(where_clause, &ast.generics);
     if let Struct(data_struct) = &ast.data {
         if let Named(FieldsNamed { named: fields, .. }) = &data_struct.fields {
             let ident = &ast.ident;
-            let split = if let Ok(separator) = separator {
-                let separator = separator.0;
-                quote!(value.split(#separator))
+            let split = if let Ok(Separator(sep)) = separator {
+                quote!(value.split(#sep))
             } else {
                 quote!(value.split_whitespace())
             };
-            let parse_fields: Vec<_> = fields
-                .iter()
-                .map(|f| {
-                    let ident = &f.ident;
-                    let parse = if f.attrs.iter().any(|a| match &a.meta {
-                        Meta::Path(path) if path.get_ident().is_some() => {
-                            path.get_ident().unwrap() == "into"
-                        }
-                        _ => false,
-                    }) {
-                        quote!(part.into())
-                    } else {
-                        quote!(
-                            match part.trim().parse() {
-                                Ok(parsed) => parsed,
-                                Err(error) => return Err(format!("Error while parsing `{}`: {:#?}", stringify!(#ident), error)),
-                            }
-                        )
-                    };
-                    quote!(
-                        let #ident = if let Some(part) = parts.next() {
-                            #parse
-                        } else {
-                            return Err(format!("Unexpected end of input while parsing {}", stringify!(#ident)));
-                        };
-                    )
-                })
-                .collect();
+            let parse_fields = parse_fields(fields);
             let fields = fields.iter().map(|f| &f.ident);
             return Ok(quote!(
                 impl #impl_generics std::str::FromStr for #ident #ty_generics #where_clause {
@@ -65,4 +41,51 @@ pub(crate) fn from_line_derive_internal(item: TokenStream) -> deluxe::Result<Tok
         }
     }
     panic!("Something went wrong");
+}
+
+fn parse_fields(fields: &Punctuated<Field, Comma>) -> Vec<TokenStream> {
+    fields
+        .iter()
+        .map(|f| {
+            let ident = &f.ident;
+            let parse = if f.attrs.iter().any(|a| match &a.meta {
+                Meta::Path(path) if path.get_ident().is_some() => {
+                    path.get_ident().unwrap() == "into"
+                }
+                _ => false,
+            }) {
+                quote!(part.into())
+            } else {
+                quote!(
+                    match part.trim().parse() {
+                        Ok(parsed) => parsed,
+                        Err(error) => return Err(format!("Error while parsing `{}`: {:#?}", stringify!(#ident), error)),
+                    }
+                )
+            };
+            quote!(
+                let #ident = if let Some(part) = parts.next() {
+                    #parse
+                } else {
+                    return Err(format!("Unexpected end of input while parsing {}", stringify!(#ident)));
+                };
+            )
+        })
+        .collect()
+}
+
+fn add_where_clause(original: Option<&WhereClause>, generics: &Generics) -> WhereClause {
+    let mut new = original.cloned().unwrap_or_else(|| WhereClause {
+        where_token: Default::default(),
+        predicates: Default::default(),
+    });
+    for param in &generics.params {
+        if let GenericParam::Type(TypeParam { ident, .. }) = param {
+            new.predicates
+                .push(parse_quote!(#ident: ::std::str::FromStr));
+            new.predicates
+                .push(parse_quote!(<#ident as std::str::FromStr>::Err: ::std::fmt::Debug));
+        };
+    }
+    new
 }
