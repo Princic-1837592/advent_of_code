4f1f441e8d0478385f05ff919026248310b8ad01
Princic-1837592 <andreapri.99@gmail.com>
2024-12-01 17:19:29 +0100

    tabs

--- 2024/day_25.rs
+++ 2024/day_25.rs
@@ -2,66 +2,66 @@
 //! https://adventofcode.com/2024/day/25/input
 
 use std::{
-    fs::read_to_string,
-    time::{Duration, Instant},
+	fs::read_to_string,
+	time::{Duration, Instant},
 };
 
 type Parsed = Vec<usize>;
 
 fn parse(_input: &str) -> Parsed {
-    vec![]
+	vec![]
 }
 
 pub mod part1 {
-    use super::Parsed;
+	use super::Parsed;
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
-    }
+	pub fn solve(_parsed: Parsed) -> usize {
+		0
+	}
 }
 
 pub mod part2 {
-    use super::Parsed;
+	use super::Parsed;
 
-    pub fn solve(_parsed: Parsed) -> usize {
-        0
-    }
+	pub fn solve(_parsed: Parsed) -> usize {
+		0
+	}
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-    let test_input = "".to_owned();
-    let puzzle_input = if test {
-        test_input
-    } else {
-        read_to_string("../inputs/2024/day_25_input.txt").unwrap()
-    };
+	let test_input = "".to_owned();
+	let puzzle_input = if test {
+		test_input
+	} else {
+		read_to_string("../inputs/2024/day_25_input.txt").unwrap()
+	};
 
-    let mut total = Duration::default();
+	let mut total = Duration::default();
 
-    let start = Instant::now();
-    let parsed = parse(&puzzle_input);
-    let elapsed = start.elapsed();
-    if verbose {
-        println!("Parsed in {:?}", elapsed);
-        total += elapsed;
-    }
+	let start = Instant::now();
+	let parsed = parse(&puzzle_input);
+	let elapsed = start.elapsed();
+	if verbose {
+		println!("Parsed in {:?}", elapsed);
+		total += elapsed;
+	}
 
-    let start = Instant::now();
-    let result = part1::solve(parsed.clone());
-    let elapsed = start.elapsed();
-    println!("{}", result);
-    println!("First part in {:?}", elapsed);
-    total += elapsed;
+	let start = Instant::now();
+	let result = part1::solve(parsed.clone());
+	let elapsed = start.elapsed();
+	println!("{}", result);
+	println!("First part in {:?}", elapsed);
+	total += elapsed;
 
-    let start = Instant::now();
-    let result = part2::solve(parsed);
-    let elapsed = start.elapsed();
-    println!("{}", result);
-    println!("Second part in {:?}", elapsed);
-    total += elapsed;
+	let start = Instant::now();
+	let result = part2::solve(parsed);
+	let elapsed = start.elapsed();
+	println!("{}", result);
+	println!("Second part in {:?}", elapsed);
+	total += elapsed;
 
-    if verbose {
-        println!("Total {:?}", total);
-    }
-    total
+	if verbose {
+		println!("Total {:?}", total);
+	}
+	total
 }
