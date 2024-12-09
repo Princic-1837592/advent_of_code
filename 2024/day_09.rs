d491def0e3ff6c034cac4834303e5f66883eed62
Princic-1837592 <andreapri.99@gmail.com>
2024-12-09 23:09:00 +0100

    day 9 part 1

--- 2024/day_09.rs
+++ 2024/day_09.rs
@@ -8,15 +8,40 @@ use std::{
 
 type Parsed = Vec<usize>;
 
-fn parse(_input: &str) -> Parsed {
-	vec![]
+fn parse(input: &str) -> Parsed {
+	input
+		.trim()
+		.chars()
+		.map(|c| c as usize - '0' as usize)
+		.collect()
 }
 
 pub mod part1 {
 	use super::Parsed;
 
-	pub fn solve(_parsed: Parsed) -> usize {
-		0
+	pub fn solve(blocks: Parsed) -> usize {
+		let mut memory = Vec::with_capacity(blocks.iter().sum());
+		for (b, block_size) in blocks.into_iter().enumerate() {
+			for _ in 0..block_size {
+				memory.push(if b % 2 == 0 { b / 2 } else { usize::MAX });
+			}
+		}
+		let mut left_i = 0;
+		let mut right_i = memory.len() - 1;
+		let mut result = 0;
+		while left_i < right_i {
+			if memory[left_i] != usize::MAX {
+				result += left_i * memory[left_i];
+				left_i += 1;
+			} else if memory[right_i] == usize::MAX {
+				right_i -= 1;
+			} else {
+				result += left_i * memory[right_i];
+				left_i += 1;
+				right_i -= 1;
+			}
+		}
+		result
 	}
 }
 
@@ -29,7 +54,7 @@ pub mod part2 {
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-	let test_input = "".to_owned();
+	let test_input = "2333133121414131402".to_owned();
 	let puzzle_input = if test {
 		test_input
 	} else {
