876a0906ed3f7a88fb1e29de51b716f8533a4401
Princic-1837592 <andreapri.99@gmail.com>
2024-12-11 09:18:05 +0100

    day 11

--- 2024/day_11.rs
+++ 2024/day_11.rs
@@ -8,28 +8,93 @@ use std::{
 
 type Parsed = Vec<usize>;
 
-fn parse(_input: &str) -> Parsed {
-	vec![]
+fn parse(input: &str) -> Parsed {
+	input
+		.split_whitespace()
+		.map(|s| s.parse().unwrap())
+		.collect()
 }
 
 pub mod part1 {
+	use std::collections::VecDeque;
+
 	use super::Parsed;
 
-	pub fn solve(_parsed: Parsed) -> usize {
-		0
+	pub fn solve(stones: Parsed) -> usize {
+		let mut queue: VecDeque<_> = stones.into_iter().map(|stone| (0, stone)).collect();
+		let mut result = 0;
+		while let Some((applied, stone)) = queue.pop_front() {
+			if applied == 25 {
+				result += 1;
+			} else if stone == 0 {
+				queue.push_back((applied + 1, 1));
+			} else if stone.ilog10() % 2 == 1 {
+				queue.push_back((applied + 1, stone % 10_usize.pow((stone.ilog10() + 1) / 2)));
+				queue.push_back((applied + 1, stone / 10_usize.pow((stone.ilog10() + 1) / 2)));
+			} else {
+				queue.push_back((applied + 1, stone * 2024));
+			}
+		}
+		result
 	}
 }
 
 pub mod part2 {
+	use std::collections::VecDeque;
+
 	use super::Parsed;
 
-	pub fn solve(_parsed: Parsed) -> usize {
-		0
+	pub fn solve(stones: Parsed) -> usize {
+		const CACHE_TARGET: usize = 40;
+		let cache: Vec<_> = (0..10_usize)
+			.map(|stone| {
+				let mut queue: VecDeque<_> = VecDeque::from([(0, stone)]);
+				let mut steps = vec![0; CACHE_TARGET + 1];
+				while let Some((applied, stone)) = queue.pop_front() {
+					steps[applied] += 1;
+					if applied == CACHE_TARGET {
+						continue;
+					} else if stone == 0 {
+						queue.push_back((applied + 1, 1));
+					} else if stone.ilog10() % 2 == 1 {
+						queue.push_back((
+							applied + 1,
+							stone % 10_usize.pow((stone.ilog10() + 1) / 2),
+						));
+						queue.push_back((
+							applied + 1,
+							stone / 10_usize.pow((stone.ilog10() + 1) / 2),
+						));
+					} else {
+						queue.push_back((applied + 1, stone * 2024));
+					}
+				}
+				steps
+			})
+			.collect();
+		const TARGET: usize = 75;
+		let mut queue: VecDeque<_> = stones.into_iter().map(|stone| (0, stone)).collect();
+		let mut result = 0;
+		while let Some((applied, stone)) = queue.pop_front() {
+			if stone < 10 && TARGET - applied <= CACHE_TARGET {
+				result += cache[stone][TARGET - applied];
+			} else if applied == TARGET {
+				result += 1;
+			} else if stone == 0 {
+				queue.push_back((applied + 1, 1));
+			} else if stone.ilog10() % 2 == 1 {
+				queue.push_back((applied + 1, stone % 10_usize.pow((stone.ilog10() + 1) / 2)));
+				queue.push_back((applied + 1, stone / 10_usize.pow((stone.ilog10() + 1) / 2)));
+			} else {
+				queue.push_back((applied + 1, stone * 2024));
+			}
+		}
+		result
 	}
 }
 
 pub fn main(test: bool, verbose: bool) -> Duration {
-	let test_input = "".to_owned();
+	let test_input = "125 17".to_owned();
 	let puzzle_input = if test {
 		test_input
 	} else {
