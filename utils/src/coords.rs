622bec70c3493572e4a4ff8c55cafd070ce2cebb
Princic-1837592 <andreapri.99@gmail.com>
2024-12-20 22:19:46 +0100

    switched to tabs

--- utils/src/coords.rs
+++ utils/src/coords.rs
@@ -2,69 +2,69 @@ use strum_macros::EnumIter;
 
 pub const CROSS_NEAR: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 pub const NEAR: [(isize, isize); 8] = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, 1),
-    (0, -1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
+	(-1, -1),
+	(-1, 0),
+	(-1, 1),
+	(0, 1),
+	(0, -1),
+	(1, -1),
+	(1, 0),
+	(1, 1),
 ];
 
 fn iter(near: &[(isize, isize)], i: isize, j: isize) -> impl Iterator<Item = (isize, isize)> + '_ {
-    near.iter().filter_map(move |(ni, nj)| {
-        i.checked_add(*ni)
-            .and_then(|i| j.checked_add(*nj).map(|j| (i, j)))
-    })
+	near.iter().filter_map(move |(ni, nj)| {
+		i.checked_add(*ni)
+			.and_then(|i| j.checked_add(*nj).map(|j| (i, j)))
+	})
 }
 
 pub fn iter_cross_near(i: isize, j: isize) -> impl Iterator<Item = (isize, isize)> {
-    iter(&CROSS_NEAR, i, j)
+	iter(&CROSS_NEAR, i, j)
 }
 
 pub fn iter_near(i: isize, j: isize) -> impl Iterator<Item = (isize, isize)> {
-    iter(&NEAR, i, j)
+	iter(&NEAR, i, j)
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, EnumIter)]
 pub enum Direction {
-    N,
-    E,
-    S,
-    W,
+	N,
+	E,
+	S,
+	W,
 }
 
 impl Direction {
-    pub const UP: Self = Self::E;
-    pub const RIGHT: Self = Self::E;
-    pub const DOWN: Self = Self::S;
-    pub const LEFT: Self = Self::W;
+	pub const UP: Self = Self::E;
+	pub const RIGHT: Self = Self::E;
+	pub const DOWN: Self = Self::S;
+	pub const LEFT: Self = Self::W;
 
-    pub const fn opposite(&self) -> Self {
-        match self {
-            Self::N => Self::S,
-            Self::E => Self::W,
-            Self::S => Self::N,
-            Self::W => Self::E,
-        }
-    }
+	pub const fn opposite(&self) -> Self {
+		match self {
+			Self::N => Self::S,
+			Self::E => Self::W,
+			Self::S => Self::N,
+			Self::W => Self::E,
+		}
+	}
 
-    pub const fn next_i(&self, i: isize, j: isize) -> (isize, isize) {
-        match self {
-            Self::N => (i - 1, j),
-            Self::E => (i, j + 1),
-            Self::S => (i + 1, j),
-            Self::W => (i, j - 1),
-        }
-    }
+	pub const fn next_i(&self, i: isize, j: isize) -> (isize, isize) {
+		match self {
+			Self::N => (i - 1, j),
+			Self::E => (i, j + 1),
+			Self::S => (i + 1, j),
+			Self::W => (i, j - 1),
+		}
+	}
 
-    pub const fn next_u(&self, i: usize, j: usize) -> (usize, usize) {
-        match self {
-            Self::N => (i.wrapping_sub(1), j),
-            Self::E => (i, j + 1),
-            Self::S => (i + 1, j),
-            Self::W => (i, j.wrapping_sub(1)),
-        }
-    }
+	pub const fn next_u(&self, i: usize, j: usize) -> (usize, usize) {
+		match self {
+			Self::N => (i.wrapping_sub(1), j),
+			Self::E => (i, j + 1),
+			Self::S => (i + 1, j),
+			Self::W => (i, j.wrapping_sub(1)),
+		}
+	}
 }
