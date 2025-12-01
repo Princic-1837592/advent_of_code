413cdeb6bf0a14c53be524bbb677047ca4c5cedc
Andrea Princic <andreapri.99@gmail.com>
2025-12-01 17:50:21 +0100

    12 days

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ cargo_toml_content = """
 [package]
 name = "advent_of_code_{year}"
 version = "0.1.0"
-authors = ["Andrea Princic <princic.1837592@studenti.uniroma1.it>"]
+authors = ["Andrea Princic <andreapri.99@gmail.com>"]
 edition = "2021"
 
 [lib]
@@ -145,90 +145,89 @@ pub mod day_{day:0>2};
 
 
 def setup_calendar(year: str, language: str = "python", auto_download=True, verbose: bool = False):
-    def rust():
-        cargo = os.path.join(year, "Cargo.toml")
-        if not os.path.exists(cargo):
-            with open(cargo, "w") as cargo_toml:
-                cargo_toml.write(cargo_toml_content.format(year=year))
-        lib = os.path.join(year, "lib.rs")
-        if not os.path.exists(lib):
-            with open(lib, "w") as lib_rs:
-                lib_rs.write(
-                    rust_lib_content.format(
-                        mods="".join(rust_lib_mod_content.format(day=day) for day in range(1, 25 + 1))
-                    )
-                )
-        for day in range(1, 25 + 1):
-            path = os.path.join(year, f"day_{day:0>2}.rs")
-            if not os.path.exists(path):
-                with open(path, "w") as src:
-                    src.write(rust_src_content.format(year=year, day=day))
-        path = os.path.join(year, "main.rs")
-        if not os.path.exists(path):
-            with open(path, "w") as main:
-                main.write(
-                    rust_main_content.format(
-                        year=year,
-                        single_days="\n        ".join(f"day_{day:0>2}, /**/" for day in range(1, 25 + 1))
-                    )
-                )
+	def rust():
+		cargo = os.path.join(year, "Cargo.toml")
+		if not os.path.exists(cargo):
+			with open(cargo, "w") as cargo_toml:
+				cargo_toml.write(cargo_toml_content.format(year=year))
+		lib = os.path.join(year, "lib.rs")
+		if not os.path.exists(lib):
+			with open(lib, "w") as lib_rs:
+				lib_rs.write(
+					rust_lib_content.format(
+						mods="".join(rust_lib_mod_content.format(day=day) for day in range(1, 25 + 1))
+					)
+				)
+		for day in range(1, 12 + 1):
+			path = os.path.join(year, f"day_{day:0>2}.rs")
+			if not os.path.exists(path):
+				with open(path, "w") as src:
+					src.write(rust_src_content.format(year=year, day=day))
+		path = os.path.join(year, "main.rs")
+		if not os.path.exists(path):
+			with open(path, "w") as main:
+				main.write(
+					rust_main_content.format(
+						year=year,
+						single_days="\n        ".join(f"day_{day:0>2}, /**/" for day in range(1, 25 + 1))
+					)
+				)
 
-    def python():
-        for day in range(1, 25 + 1):
-            path = os.path.join(year, f"day_{day:0>2}.py")
-            if not os.path.exists(path):
-                with open(path, "w") as src:
-                    src.write(python_src_content.format(year=year, day=day))
+	def python():
+		for day in range(1, 12 + 1):
+			path = os.path.join(year, f"day_{day:0>2}.py")
+			if not os.path.exists(path):
+				with open(path, "w") as src:
+					src.write(python_src_content.format(year=year, day=day))
 
-    def inputs():
-        directory = os.path.join("inputs", year)
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        session = "None"
-        if auto_download:
-            config = ConfigParser()
-            config.read("local_config.ini")
-            session = config["auto_download"]["session"]
-        keep_downloading = True
-        for day in range(1, 25 + 1):
-            path = os.path.join(directory, f"day_{day:0>2}_input.txt")
-            if not os.path.exists(path):
-                with open(path, "w") as day_input:
-                    if auto_download and keep_downloading:
-                        response = get(
-                            f"https://adventofcode.com/{year}/day/{day}/input",
-                            cookies={"session": session},
-                            headers={
-                                "User-Agent": "https://github.com/Princic-1837592/advent_of_code/blob/main/setup.py"
-                                              " by princic.1837592@studenti.uniroma1.it"
-                            },
-                        )
-                        if response.status_code == 200:
-                            day_input.write(response.text.rstrip("\n"))
-                            if verbose:
-                                print(f"Downloaded day {day}")
-                        else:
-                            keep_downloading = False
-                            print(
-                                f"Failed to download day {day} with error code "
-                                f"{response.status_code} and response: {response.text}. Quitting"
-                            )
-            sleep(0.2)
+	def inputs():
+		directory = os.path.join("inputs", year)
+		if not os.path.exists(directory):
+			os.mkdir(directory)
+		session = "None"
+		if auto_download:
+			config = ConfigParser()
+			config.read("local_config.ini")
+			session = config["auto_download"]["session"]
+		keep_downloading = True
+		for day in range(1, 12 + 1):
+			path = os.path.join(directory, f"day_{day:0>2}_input.txt")
+			if not os.path.exists(path):
+				with open(path, "w") as day_input:
+					if auto_download and keep_downloading:
+						response = get(
+							f"https://adventofcode.com/{year}/day/{day}/input",
+							cookies={"session": session},
+							headers={
+								"User-Agent": "https://github.com/Princic-1837592/advent_of_code/blob/main/setup.py by andreapri.99@gmail.com"
+							},
+						)
+						if response.status_code == 200:
+							day_input.write(response.text.rstrip("\n"))
+							if verbose:
+								print(f"Downloaded day {day}")
+						else:
+							keep_downloading = False
+							print(
+								f"Failed to download day {day} with error code "
+								f"{response.status_code} and response: {response.text}. Quitting"
+							)
+			sleep(0.2)
 
-    if not os.path.exists(year):
-        if verbose:
-            print("Creating directory")
-        os.mkdir(year)
-    if verbose:
-        print("Creating source files")
-    if language == "python":
-        python()
-    elif language == "rust":
-        rust()
-    if verbose:
-        print("Creating inputs")
-    inputs()
+	if not os.path.exists(year):
+		if verbose:
+			print("Creating directory")
+		os.mkdir(year)
+	if verbose:
+		print("Creating source files")
+	if language == "python":
+		python()
+	elif language == "rust":
+		rust()
+	if verbose:
+		print("Creating inputs")
+	inputs()
 
 
 if __name__ == "__main__":
-    setup_calendar("2024", "rust", False, True)
+	setup_calendar("2025", "rust", False, True)
