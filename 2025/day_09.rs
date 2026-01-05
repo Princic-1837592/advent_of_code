3efec9f1665440a68570ede2e07d15a301ee2732
Andrea Princic <andreapri.99@gmail.com>
2026-01-05 17:11:51 +0100

    day 9 part 2

--- 2025/day_09.rs
+++ 2025/day_09.rs
@@ -49,10 +49,82 @@ pub mod part1 {
 }
 
 pub mod part2 {
-	use super::Parsed;
+	use super::{Parsed, Point};
 
-	pub fn solve(_parsed: Parsed) -> usize {
-		0
+	#[derive(Copy, Clone, Debug)]
+	struct Segment {
+		a: Point,
+		b: Point,
+	}
+
+	pub fn solve(mut points: Parsed) -> usize {
+		let mut verticals = Vec::with_capacity(points.len() / 2);
+		let mut horizontals = Vec::with_capacity(points.len() / 2);
+		let mut min_x = usize::MAX;
+		let mut min_y = usize::MAX;
+		let mut current = points[0];
+		points.push(points[0]);
+		for &next in &points[1..] {
+			let segment = Segment {
+				a: current,
+				b: next,
+			};
+			if current.x == next.x {
+				min_y = min_y.min(current.y.abs_diff(next.y));
+				verticals.push(segment);
+			} else {
+				min_x = min_x.min(current.x.abs_diff(next.x));
+				horizontals.push(segment);
+			}
+			current = next;
+		}
+		points.pop();
+		let mut max = 0;
+		for (i, &Point { x: xi, y: yi }) in points.iter().enumerate() {
+			'next_point: for &Point { x: xj, y: yj } in &points[i + 1..] {
+				let x1 = xi.min(xj);
+				let x2 = xi.max(xj);
+				let y1 = yi.min(yj);
+				let y2 = yi.max(yj);
+				for &Segment {
+					a: Point { x: sx, y: sy1 },
+					b: Point { y: sy2, .. },
+				} in &verticals
+				{
+					let (sy1, sy2) = (sy1.min(sy2), sy1.max(sy2));
+					if (x1 < sx && sx < x2) && (sy1 < y1 && y1 < sy2) {
+						continue 'next_point;
+					}
+					if (x1 < sx && sx < x2) && (sy1 < y2 && y2 < sy2) {
+						continue 'next_point;
+					}
+					if (x1 < sx && sx < x2) && (y1 < sy1 && sy2 < y2) {
+						continue 'next_point;
+					}
+				}
+				for &Segment {
+					a: Point { x: sx1, y: sy },
+					b: Point { x: sx2, .. },
+				} in &horizontals
+				{
+					let (sx1, sx2) = (sx1.min(sx2), sx1.max(sx2));
+					if (y1 < sy && sy < y2) && (sx1.min(sx2) < x1 && x1 < sx1.max(sx2)) {
+						continue 'next_point;
+					}
+					if (y1 < sy && sy < y2) && (sx1.min(sx2) < x2 && x2 < sx1.max(sx2)) {
+						continue 'next_point;
+					}
+					if (y1 < sy && sy < y2) && (x1 < sx1 && sx2 < x2) {
+						continue 'next_point;
+					}
+				}
+				let area = (xi.abs_diff(xj) + 1) * (yi.abs_diff(yj) + 1);
+				if area > max {
+					max = area;
+				}
+			}
+		}
+		max
 	}
 }
 
